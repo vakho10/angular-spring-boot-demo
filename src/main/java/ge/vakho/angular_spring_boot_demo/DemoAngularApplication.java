e73f7b63b4197333e37d256c02fca777cc7866dd
unknown <vakho10@gmail.com>
2019-12-08 22:27:09 +0400

    First commit.

---
+++ src/main/java/ge/vakho/angular_spring_boot_demo/DemoAngularApplication.java
@@ -0,0 +1,13 @@
+package ge.vakho.angular_spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAngularApplication.class, args);
+	}
+
+}
